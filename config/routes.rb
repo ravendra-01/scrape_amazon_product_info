c5ed188325c56308e310ec0aa7e6f08be369ddf0
rkpatel <rk.ravendra01@gmail.com>
2024-04-04 12:35:00 +0530

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
-  root 'home#index'
+  # root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "scrape_details#product_details"
   get '/product_details', to: 'scrape_details#product_details'
   post '/product_details/scrape', to: 'scrape_details#scrape'
   get '/product_details/search_product', to: 'scrape_details#search_product'
